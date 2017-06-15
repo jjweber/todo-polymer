e660491c8f50acbb8592c806c3c0435d1e7e49c9
Your Name <you@example.com>
2017-06-15 17:24:32 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('images', function(){
+  gulp.src('images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/assets/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/assets/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(concat('main.js'))
+    .pipe(babel())
+    .pipe(gulp.dest('dist/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("src/assets/**/*.scss", ['styles']);
+  gulp.watch("src/**/*.js", ['scripts']);
+  gulp.watch("src/**/*.html", ['bs-reload']);
+});
